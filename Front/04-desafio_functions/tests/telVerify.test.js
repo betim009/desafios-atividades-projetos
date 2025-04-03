9579f3fbc568951996b375d3f88e04c9f0463688
unknown <betim009@gmail.com>
2025-04-02 21:25:59 -0300

    update

--- Front/04-desafio_functions/tests/telVerify.test.js
+++ Front/04-desafio_functions/tests/telVerify.test.js
@@ -2,10 +2,14 @@ const { telVerify } = require("../scripts/scripts.js");
 
 describe('Testando a função "telVerify"', () => {
     test('Deve retornar false enviando um número com mais de 10 digitos', () => {
-        expect(telVerify('27999888777665')).toBe(false);
+        expect(telVerify('27999082744')).toBe(false);
     });
 
-    test('Deve retonar false se enviar um número', () => {
+    test('Deve retornar false enviando um número com menos de 10 digitos', () => {
+        expect(telVerify('27999084')).toBe(false);
+    });
+
+    test('Enviando "2799082744" e recebendo "(27) 99082744"', () => {
         expect(telVerify('2799082744')).toBe("(27) 99082744");
     });
 });q
